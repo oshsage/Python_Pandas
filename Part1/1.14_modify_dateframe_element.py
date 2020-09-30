9a430abfac9c0870e447bf698bcae2b799d5389d
oshsage <oh12sung@naver.com>
2020-09-30 23:54:16 +0900

    20200930

---
+++ Part1/1.14_modify_dateframe_element.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+import pandas as pd
+
+exam_data = {'이름': ['서준', '우현', '인아'],
+             '수학': [90, 80, 70],
+             '영어': [98, 89, 95],
+             '음악': [85, 95, 100],
+             '체육': [100, 90, 90]}
+
+df = pd.DataFrame(exam_data)
+
+# '이름' 열을 새로운 인덱스로 지정, df 원본객체에 변경사항 반영
+df.set_index('이름', inplace=True)
+print(df)
+print('\n')
+
+# 서준의 체육점수
+df.iloc[0][3] = 80
+print(df)
+print('\n')
+
+df.loc['서준']['체육'] = 90
+print(df)
+print('\n')
+
+df.loc['서준', '체육'] = 100
+print(df)
+print('\n')
+
+df.loc['서준', ['음악', '체육']] = 50
+print(df)
+print('\n')
+
+df.loc['서준', ['음악', '체육']] = 100, 50
+print(df)
